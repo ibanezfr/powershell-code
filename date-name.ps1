32947850f522bfdfbbd8cfe6003ee4d3c5ca26eb
ibanezfr <francoibanez.dev@gmail.com>
2024-09-15 23:06:15 -0300

    first pwsh code

---
+++ date-name.ps1
@@ -0,0 +1,4 @@
+$date = Read-Host "What is today's date"
+$name = Read-Host "Please enter your name"
+Write-OutPut "Today's date is $date"
+Write-OutPut "Today is the day $name began a PowerShell programming journey."
